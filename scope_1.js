0089cc0c80e95462df7ad719fe91bbd259e25237
DrInnovatio <turing1723@gmail.com>
2022-05-29 22:03:01 +0900

    difference between let and  var in 'if statement';

--- scope_1.js
+++ scope_1.js
@@ -13,7 +13,7 @@ home();
 //function 안에 있는 지역 변수를 먼저 찾고 그게 없으면 전역 변수를 찾는다. scope chain의 의미.
 
 
-function hom2() {
+function home2() {
   var homeVar = "in the house";
   for (let i = 0; i < 100; i++){
     
@@ -25,3 +25,10 @@ home2();
 
 // let 을 쓰면 for loop 안에서만 유효하다. 에러발생.
 
+if (true ) {
+  //let my = "my love habok"
+  var my = "my love habok"
+}
+console.log(my);
+
+// difference between let and  var in 'if statement';

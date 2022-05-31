8aca8b62c710d9612b9675a8c25620e38a68e495
DrInnovatio <turing1723@gmail.com>
2022-05-31 23:09:23 +0900

    difference between const and var

---
+++ scope_3.js
@@ -0,0 +1,14 @@
+function home() {
+  var homename = "my house";
+  homename = "not my house"
+  console.log(homename);
+}
+
+home();
+
+//difference between const and var
+
+// HOME_NAME => const naming.
+
+// const 를 기본으로 사용. 변경시 let을 사용. var 는 자주 쓰지 않는다.
+
